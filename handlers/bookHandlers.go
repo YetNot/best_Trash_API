031c3080997b74944f5a45c4adff271c23b33f47
Kirill Glinskikh <glinskix24@bk.ru>
2023-09-27 11:22:05 +0300

    end

--- handlers/bookHandlers.go
+++ handlers/bookHandlers.go
@@ -67,16 +67,60 @@ func UpdateBookById(writer http.ResponseWriter, request *http.Request) {
 	}
 
 	oldBook, ok := models.FindBookId(id)
+	var newBook models.Book
 	if !ok {
 		log.Println("book not found in database:", id)
 		writer.WriteHeader(404)
 		msg := models.Message{Message: "book with that ID does not exists in database"}
 		json.NewEncoder(writer).Encode(msg)
-	} else {
-
+		return
 	}
+	err = json.NewDecoder(request.Body).Decode(&newBook)
+	if err != nil {
+		msg := models.Message{Message: "provideed json file is invalid"}
+		writer.WriteHeader(400)
+		json.NewEncoder(writer).Encode(msg)
+		return
+	}
+
+	for ind, elem := range models.DB {
+		if elem == oldBook {
+			models.DB[ind] = newBook
+		}
+	}
+
+	msg := models.Message{Message: "successfully updating request item"}
+	json.NewEncoder(writer).Encode(msg)
+
 }
 
 func DeleteBookById(writer http.ResponseWriter, request *http.Request) {
 	initHeaders(writer)
+	log.Println("Deleting book...")
+	id, err := strconv.Atoi(mux.Vars(request)["id"])
+	if err != nil {
+		log.Println("error while parsing:", err)
+		writer.WriteHeader(400)
+		msg := models.Message{Message: "do not use parametr ID"}
+		json.NewEncoder(writer).Encode(msg)
+		return
+	}
+
+	book, ok := models.FindBookId(id)
+	if !ok {
+		log.Println("book not found in database:", id)
+		writer.WriteHeader(404)
+		msg := models.Message{Message: "book with that ID does not exists in database"}
+		json.NewEncoder(writer).Encode(msg)
+		return
+	}
+	
+	for ind, elem := range models.DB {
+		if elem == book {
+			models.DB = append(models.DB[:ind], models.DB[ind+1:]...)
+		}
+	}
+
+	msg := models.Message{Message: "successfully deleted request item"}
+	json.NewEncoder(writer).Encode(msg)
 }
